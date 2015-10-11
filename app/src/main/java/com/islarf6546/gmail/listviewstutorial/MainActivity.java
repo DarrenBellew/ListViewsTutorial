898f9fdb9e3dc39fa5df2b510cb8f63100bb950b
islarf <islarf6546@gmail.com>
2015-10-11 14:53:40 +0100

    Test New Commit

--- app/src/main/java/com/islarf6546/gmail/listviewstutorial/MainActivity.java
+++ app/src/main/java/com/islarf6546/gmail/listviewstutorial/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    //TEST NEW THINGY
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
